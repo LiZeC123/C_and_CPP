44c878f5091aad192f6cd79e122b05ce0896d353
LiZeC <tomgreem123@gmail.com>
2016-03-10 20:55:01 +0800

    堆栈

---
+++ Date_Structures/Stack/Stack.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "Stack.h"
+
+static void CopyToNode(Item item,Node * pnode);
+
+void InitializeStack(Stack * pstack){
+	pstack->head = NULL;
+	pstack->size = 0;
+	pstack->end  = NULL;
+}
+
+bool StackIsEmpty(Stack * pstack){
+	if(pstack->size = 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+ 
+bool StackIsFull(Stack * pstack){
+	return pstack->size == MAX_SIZE;	
+}
+
+unsigned int StackItemCount(const Stack * pstack){
+	return pstack->size;
+}
+
+bool Push(Item item,Stack * pstack){
+	if(StackIsFull(pstack)){
+		return false;
+	}
+	
+	Node * pnew;
+	pnew=(Node *)malloc(sizeof(Node));
+
+	
+	CopyToNode(item,pnew);
+	pnew->next=NULL;
+	
+	if(pstack->end == NULL){
+		pstack->head = pnew;
+		pstack->end =  pnew;
+	}
+	else{
+		pstack->end->next = pnew;
+		pstack->end = pnew;
+	}
+	pstack->size++;
+	return true;
+}
+
+bool Pop(Stack * pstack){
+	if(StackIsEmpty(pstack)){
+		return false;
+	}
+	
+	if(pstack->size == 1){
+		free(pstack->end);
+		pstack->head = NULL;
+		pstack->end  = NULL;	
+	}
+	else{
+		int max_num = StackIsEmpty(pstack);
+		Node * this_node;
+		Node * pre_node = pstack->head;
+		int count=1;
+	
+		while(count < max_num -1){
+			pre_node = pre_node->next;
+			count++;
+		}
+		
+		this_node = pre_node->next;
+		free(this_node);
+		pre_node->next = NULL;
+		pstack->end = pre_node;
+	}
+
+	pstack->size--;
+	return true;
+}
+
+static void CopyToNode(Item item,Node * pnode){
+	pnode->item = item;
+}
---
+++ Date_Structures/Stack/Stack.h
@@ -0,0 +1,37 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stdbool.h>
+
+#define MAX_SIZE 10
+
+typedef struct item{
+	int data;
+}Item;
+
+
+typedef struct node{
+	Item item;
+	struct node * next;
+}Node;
+
+typedef struct stack{
+	Node * head;
+	int size;
+	Node * end;
+}Stack;
+
+
+void InitializeStack(Stack * pstack);
+
+bool StackIsEmpty(Stack * pstack);
+
+bool StackIsFull(Stack * pstack);
+
+unsigned int StackItemCount(const Stack * pstack);
+
+bool Push(Item item,Stack * pstack);
+
+bool Pop(Stack * pstack);
+
+#endif
